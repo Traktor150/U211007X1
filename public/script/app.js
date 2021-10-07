297b54690e71117c357fc9af5d52b3f4468a4dff
William <williambecker321@gmail.com>
2021-10-07 12:49:22 +0200

    Har gort exersise1

--- public/script/app.js
+++ public/script/app.js
@@ -1,3 +1,25 @@
 'use strict';
 
 console.log('I am also Alive.');
+
+const dataURL = '../data/exercise1.json';
+const req = new XMLHttpRequest();
+req.open('GET', dataURL);
+req.responseType = 'json';
+req.send()
+
+req.onload = () => {
+    const data = req.response;
+    const head_data = data.headline;
+    const intro_data = data.intro;
+
+    const head = document.createElement('h1');
+    head.textContent = head_data;
+    
+    const intro = document.createElement('p');
+    intro.textContent = intro_data;
+
+    document.getElementById('Exercise1').appendChild(head);
+    document.getElementById('Exercise1').appendChild(intro);
+}
+
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 8084;
 
+server.use(express.static('public'));
 
 
 const routerDefault = require('./router/default');
 server.use('/', routerDefault);
 
-server.use(express.static('public'));
 
 server.listen(port);
 
